f382807743ad579a00fe6bdf24b606f6078b0741
drbln <43165382+drbln@users.noreply.github.com>
2025-01-27 16:02:16 +0300

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,31 +1,47 @@
+import logging
 from func import *
 from var import *
 import time
 import pyautogui
 import threading
 
-while True:
-    try:
-        driver.execute_cdp_cmd('Network.clearBrowserCache', {})
-        driver.execute_cdp_cmd('Network.clearBrowserCookies', {})
-        driver.maximize_window()
 
-        thread = threading.Thread(target = first_window)
+class Reg:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clear_browser_data(self):
+        """Очистка кеша и куки браузера."""
+        self.driver.execute_cdp_cmd('Network.clearBrowserCache', {})
+        self.driver.execute_cdp_cmd('Network.clearBrowserCookies', {})
+        self.driver.maximize_window()
+
+    def run_first_window(self):
+        """Запуск первого окна в отдельном потоке."""
+        thread = threading.Thread(target=first_window)
         thread.start()
-
         time.sleep(10)
         pyautogui.press('enter')
 
+    def perform_actions(self):
+        """Выполнение основной последовательности действий."""
         button_click(button2)
         button_click(button3)
         button_click(button4)
         password_field(button5, passw1)
         button_click(button3)
         button_click(button6)
-
         time.sleep(10)
 
-        break
-    except Exception as e:
-        driver.refresh()
-        time.sleep(5)
+    def execute(self):
+        """Основной метод для выполнения действий с обработкой ошибок."""
+        while True:
+            try:
+                print("Запуск автоматизации...")
+                self.clear_browser_data()
+                self.run_first_window()
+                self.perform_actions()
+                break
+            except Exception as e:
+                self.driver.refresh()
+                time.sleep(5)
