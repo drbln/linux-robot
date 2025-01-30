e94418d6fd14826198aa7ddd97343530a8c52006
drbln <43165382+drbln@users.noreply.github.com>
2025-01-30 09:01:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from reg import Reg
+from reg import Post
 from var import driver
 
 if __name__ == "__main__":
     automation = Reg(driver)
     automation.execute()
+
+    posting = Post(driver)
+    posting.perform_actions()
