f93d8bf81be457e7d5f5fb8f1064a4482c2ce109
drbln <43165382+drbln@users.noreply.github.com>
2025-01-30 09:01:15 +0300

    Добавлена логика размещения извещений, не работает последнее нажатие
    последнее нажатие происходит но скрипт падает в ошибку и действие не совершается

--- reg.py
+++ reg.py
@@ -5,6 +5,7 @@ import pyautogui
 import threading
 
 
+
 class Reg:
     def __init__(self, driver):
         self.driver = driver
@@ -43,3 +44,32 @@ class Reg:
             except Exception as e:
                 self.driver.refresh()
                 time.sleep(5)
+
+
+class Post:
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def perform_actions(self):
+        #Выполнение основной последовательности действий
+        follow_the_link(link2)
+        button_click(button7)
+        button_click(button10)
+        skip_warnings()
+
+        button_click(button11)
+        time.sleep(10)
+        switch_New_window()
+        time.sleep(10)
+        driver.find_element(By.ID, "sign_and_submit_btn").click()
+        time.sleep(10)
+        pyautogui.press('enter')
+        button555 = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.jquery-msgbox > div > form > div.jquery-msgbox-buttons > button:nth-child(2)"))
+        )
+
+        # Кликаем через JavaScript
+        driver.execute_script("arguments[0].click();", button555)
+        time.sleep(100)
+
+  
