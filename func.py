3018c7f44b5c22e343faeb1c8f723b9dc278a615
drbln <43165382+drbln@users.noreply.github.com>
2025-01-30 08:58:47 +0300

    Добавление новых функций для размещения извещений

--- func.py
+++ func.py
@@ -1,6 +1,8 @@
 from var import *
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 def first_window():
     driver.get(link1)
@@ -11,3 +13,23 @@ def button_click(button):
 
 def password_field(button, passw):
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, button))).send_keys(passw)
+
+def follow_the_link(link):
+    driver.get(link)
+
+def switch_New_window():
+    main_window_handle = driver.current_window_handle
+    all_window_handles = driver.window_handles
+    for handle in all_window_handles:
+        if handle != main_window_handle:
+            driver.switch_to.window(handle)
+            break
+
+def skip_warnings():
+    while True:
+            try:
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#dialogDiv > div:nth-child(3) > a:nth-child(1) > input"))).click()
+            except Exception as e:
+                print(f"Произошла ошибка: {e}")
+                break
+
