0231bf6074514dffd3984d7feefc3f21ea167f0c
drbln <43165382+drbln@users.noreply.github.com>
2025-02-03 11:36:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from reg import Reg, Post
 from var import driver
-import time
 
 if __name__ == "__main__":
-    while True:
+
         automation = Reg(driver)
         automation.execute()
 
         posting = Post(driver)
         posting.execute()
-
         driver.quit()
-        time.sleep(900)
-
