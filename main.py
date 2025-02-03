995132927a75d1122bbda4ea79881f6be534c0b5
Oleg <r_oleg94@mail.ru>
2025-02-03 08:37:38 +0300

    Добавил бесконечный цикл с остановкой на 15 минут

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from reg import Reg, Post
 from var import driver
+import time
 
 if __name__ == "__main__":
-    automation = Reg(driver)
-    automation.execute()
+    while True:
+        automation = Reg(driver)
+        automation.execute()
 
-    posting = Post(driver)
-    posting.execute()
+        posting = Post(driver)
+        posting.execute()
+
+        time.sleep(900)
+
