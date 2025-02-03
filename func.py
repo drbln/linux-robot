6f0af8b285885aa266fed5d7ad11bace1a0a6e8e
drbln <43165382+drbln@users.noreply.github.com>
2025-02-03 11:35:33 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,39 +1,85 @@
+from func import *
 from var import *
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+import time
+import pyautogui
+import threading
 
 
-def first_window():
-    driver.get(link1)
-    driver.find_element(By.XPATH, button1).click()
 
-def button_click(button):
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, button))).click()
+class Reg:
+    def __init__(self, driver):
+        self.driver = driver
 
-def password_field(button, passw):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, button))).send_keys(passw)
+    def clear_browser_data(self):
+        """Очистка кеша и куки браузера."""
+        self.driver.execute_cdp_cmd('Network.clearBrowserCache', {})
+        self.driver.execute_cdp_cmd('Network.clearBrowserCookies', {})
+        self.driver.maximize_window()
 
-def follow_the_link(link):
-    driver.get(link)
+    def run_first_window(self):
+        """Запуск первого окна в отдельном потоке."""
+        thread = threading.Thread(target=first_window)
+        thread.start()
+        time.sleep(20)
+        pyautogui.press('enter')
 
-def switch_New_window():
-    main_window_handle = driver.current_window_handle
-    all_window_handles = driver.window_handles
-    for handle in all_window_handles:
-        if handle != main_window_handle:
-            driver.switch_to.window(handle)
-            break
+    def perform_actions(self):
+        """Выполнение основной последовательности действий."""
+        button_click(button2)
+        button_click(button3)
+        button_click(button4)
+        password_field(button5, passw1)
+        button_click(button3)
+        button_click(button6)
+        time.sleep(10)
 
-def skip_warnings():
-    while True:
+    def execute(self):
+        """Основной метод для выполнения действий с обработкой ошибок."""
+        while True:
             try:
-                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#dialogDiv > div:nth-child(3) > a:nth-child(1) > input"))).click()
+                self.clear_browser_data()
+                self.run_first_window()
+                self.perform_actions()
+                break
             except Exception as e:
-                print(f"Произошла ошибка: {e}")
+                self.driver.refresh()
+                time.sleep(5)
+
+
+class Post:
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def perform_actions(self):
+        #Выполнение основной последовательности действий
+#        follow_the_link(link2)
+#        button_click(button7)
+        button_click(button10)
+        skip_warnings()
+        button_click(button11)
+        time.sleep(10)
+        switch_New_window()
+        time.sleep(10)
+        driver.find_element(By.ID, "sign_and_submit_btn").click()
+        time.sleep(10)
+        pyautogui.press('enter')
+        button_click(button13)
+        switch_New_window()
+        time.sleep(20)
+
+    def execute(self):
+        follow_the_link(link2)
+        while True:
+            if button_check(driver, button7):
+                try:
+                    button_click(button7)
+                    thread = threading.Thread(target=self.perform_actions)
+                    thread.start()
+                    thread.join()
+                except:
+                    self.driver.refresh()
+                    time.sleep(5)
+            else: 
                 break
 
-def button_check(driver, button):
-    elements = driver.find_elements(By.XPATH, button)
-    return len(elements) > 0
-
+  
